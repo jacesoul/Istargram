167d4255e2a35dd629cd4c41c49dea16e2de2383
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-21 03:21:52 +0900

    ADD : 게시물 get & 댓글등록

--- users/views.py
+++ users/views.py
@@ -3,21 +3,18 @@
 
 import json, re
 
-from django.http        import JsonResponse
-from django.views       import View
+from django.http            import JsonResponse
+from django.views           import View
 from django.core.exceptions import ValidationError
 
 from .models       import Signup
 
 def email_check(email): 
     return bool(re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',email))
-
 def password_check(pw): 
     return bool(re.match('.{8,45}', pw))
 
-
 class UsersView(View):
-
     def post(self, request): 
 
         try:
@@ -25,9 +22,7 @@ class UsersView(View):
             email = data['email']
             password    = data['password']
             
-
             if email and password:
-
                 if Signup.objects.filter(email=email).exists():
                     return JsonResponse({'MESSAGE': 'EMAIL_ALREADY_EXISTS'}, status=400)
 
@@ -36,37 +31,34 @@ class UsersView(View):
 
                 # if '@' not in email or '.' not in email:
                 #     return JsonResponse({'MESSAGE' : 'INVALID_EMAIL'},status=400)
-
                 # if len(password) < 8: 
                 #     return JsonResponse({'MESSAGE' : 'INVALID_PASSWORD'},status=400)
-
                 if not email_check(email):
                     return JsonResponse({'MESSAGE' : 'INVALID_EMAIL'},status=400)
 
                 if not password_check(password):
                     return JsonResponse({'MESSAGE' : 'INVALID_PASSWORD'},status=400)
-
+                    
                 signup = Signup.objects.create(
                     email    = email,
                     password = password
                 )
-
             return JsonResponse({'message': 'SUCCESS'}, status=200)
-
         
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
          
 
 class LoginsView(View):  
-
     def post(self, request):
+
         try: 
-            data  = json.loads(request.body)
-            email = data['email']
+            data        = json.loads(request.body)
+            email       = data['email']
             password    = data['password']
             
             if email and password: 
+
                 if not Signup.objects.filter(email=email).exists():
                     return JsonResponse({'MESSAGE':'INVALID_USER'},status=401)
 
@@ -74,8 +66,11 @@ class LoginsView(View):
                     return JsonResponse({'MESSAGE':'INVALID_USER'},status=401)
 
                 return JsonResponse({"MESSAGE":"SUCCESS"},status=200)
-
         
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+    
+
+
 
