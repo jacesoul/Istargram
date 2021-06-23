84917fe5c410d2fa6606377a41d9a1f8724d75f2
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-23 20:01:07 +0900

    updated : new migrations -chage comment name -tested 'self reference' and 'manyTomanyField'

--- postings/models.py
+++ postings/models.py
@@ -12,10 +12,10 @@ class User(models.Model):
         db_table = 'users'
 
 class Post(models.Model): 
-    name = models.ForeignKey('User', on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add =True)
-    updated_at=models.DateTimeField(auto_now =True)
-    image_url=models.URLField(max_length=200)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add =True)
+    updated_at = models.DateTimeField(auto_now =True)
+    image_url = models.URLField(max_length=200)
     title=models.CharField(max_length=100, default='awesome')
     content=models.CharField(max_length=1000, default='Hello World!')
     class Meta:
@@ -23,25 +23,23 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    comment_post = models.ForeignKey('User',on_delete=models.CASCADE, related_name='comment_user',null=True)
-    created_at=models.DateTimeField(auto_now_add =True)
-    updated_at=models.DateTimeField(auto_now =True)
+    user = models.ForeignKey('User',on_delete=models.CASCADE, related_name='comment_user',null=True)
+    created_at =models.DateTimeField(auto_now_add =True)
+    updated_at =models.DateTimeField(auto_now =True)
     content = models.CharField(max_length=1000)
-    comment_id = models.ForeignKey('Comment',on_delete=models.CASCADE, null=True)
+    comment = models.ForeignKey('self',on_delete=models.CASCADE, null=True, related_name = "post_of_post")
     class Meta:
         db_table = 'comments'
 
 class Like(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    like_post = models.ForeignKey('User', on_delete=models.CASCADE, related_name = 'like_user',null=True)
-    like = models.BooleanField(null=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name = 'like_user',null=True)
     class Meta:
         db_table = 'likes'
 
 class Follow(models.Model):
     user = models.ForeignKey('User',on_delete=models.CASCADE, related_name='follower',null=True)
     follow_user = models.ForeignKey('User',on_delete=models.CASCADE, related_name='followed',null=True)
-    follow = models.BooleanField(null=True)
     class Meta: 
         db_table = 'follows'
 
